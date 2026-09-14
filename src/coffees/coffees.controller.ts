a6d45b62471fade0af7526fe9d91d10a2b809abb
Alfredo Stanquini Neto <stanquini@gmail.com>
2026-09-14 20:39:44 -0300

    feat(coffees): refactor coffee and flavor entities, add pagination and update DTOs
    - Updated `CoffeesModule` to include TypeORM integration with PostgreSQL.
    - Refactored `CoffeesService` to use TypeORM for data access, including methods for finding, creating, updating, and removing coffee entries.
    - Introduced `Flavor` entity and established a many-to-many relationship with `Coffee`.
    - Added `PaginationQueryDto` for handling pagination in the `findAll` method.
    - Updated HTTP requests in `requests.http` to reflect new API structure.
    - Created migration files for schema changes, including adding a description field to the coffee entity.
    - Enhanced error handling with NotFoundException for better user feedback.
    - Updated `CoffeesController` to utilize new DTOs and improved service methods.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,28 +10,26 @@ import {
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/create-coffee.dto/update-coffee.dto copy';
+import { UpdateCoffeeDto } from './dto/create-coffee.dto/update-coffee.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery: { limit?: number; offset?: number }) {
-    // const { limit, offset } = paginationQuery;
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    console.log(typeof id);
     return this.coffeesService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    console.log(createCoffeeDto instanceof CreateCoffeeDto);
-    return this.coffeesService.create(createCoffeeDto) as CreateCoffeeDto;
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
---
+++ src/migrations/1789151184710-SchemaSync.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SchemaSync1789151184710 implements MigrationInterface {
+  name = 'SchemaSync1789151184710'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "coffee" ADD "description" character varying`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "coffee" DROP COLUMN "description"`);
+  }
+}
